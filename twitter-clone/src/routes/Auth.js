bc6d8c4aa7699d39bbdaaec1b7402efc87f79434
Taeeun Kim <taeeun.kim@outlook.com>
2022-03-05 10:19:21 +0100

    Remove project

--- twitter-clone/src/routes/Auth.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-// export default () => <span>Auth</span>;
-const Auth = () => <span>Auth</span>;
-export default Auth;
--- twitter-clone/src/routes/EditProfile.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const Edit = () => <span>EditProfile</span>;
-export default Edit;
