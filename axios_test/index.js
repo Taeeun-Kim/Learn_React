47b42fb0b6d19a6657ec512a22176d9e878017a0
Taeeun Kim <taeeun.kim@outlook.com>
2022-03-13 14:49:14 +0100

    Update index.js

--- axios_test/index.js
+++ axios_test/index.js
@@ -2,11 +2,17 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 axios
-  .get('test')
+  .get(
+    'https://www.google.de/webhp?hl=ko&sa=X&ved=0ahUKEwjjsqr0msP2AhXCSvEDHfc0D9MQPAgI'
+  )
   .then((response) => {
-    const $ = cheerio.load(response.data);
-    data = $('#test').text();
-    console.log(data);
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var dateString = year + '-' + month + '-' + day;
+
+    console.log(dateString);
   })
   .catch((Error) => {
     console.log(Error);
