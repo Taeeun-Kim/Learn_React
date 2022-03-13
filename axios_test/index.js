57969c591fa52b5713d41321e36dca1aa6434c3b
Taeeun Kim <taeeun.kim@outlook.com>
2022-03-13 11:25:54 +0100

    Update index.js

--- axios_test/index.js
+++ axios_test/index.js
@@ -1,2 +1,13 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+axios
+  .get('test')
+  .then((response) => {
+    const $ = cheerio.load(response.data);
+    data = $('#test').text();
+    console.log(data);
+  })
+  .catch((Error) => {
+    console.log(Error);
+  });
