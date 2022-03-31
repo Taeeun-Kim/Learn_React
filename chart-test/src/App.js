91d98e0d4cb99711e4180c728f6780aaba821175
Taeeun Kim <taeeun.kim@outlook.com>
2022-03-31 18:33:12 +0200

    test chart in react

---
+++ chart-test/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { Bar } from 'react-chartjs-2';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        ttqwipfqwfwq
+        <Bar
+          data={{}}
+          height={400}
+          width={600}
+          // options={{
+          //   maintainAspectRatio: false,
+          // }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
